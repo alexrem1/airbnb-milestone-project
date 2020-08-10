e23eb0cc52469ef6d35e5ea1de644618dd3338b1
Alexander Roberts <remidy@live.co.uk>
2020-08-10 14:29:41 +0000

    Added static(css,logo,propertyphotos) and templates(chooseus.html) folders. Landing page created

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for, session
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/choose_us")
 def choose_us():
-     return "It's working"
+     return render_template("chooseus.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
